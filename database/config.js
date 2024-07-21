0e0dcbd29fdb8c4531440c2060db89d0292a6794
EdisonDSG <edesousaguita@gmail.com>
2024-07-21 09:04:23 -0300

    Second commit

--- database/config.js
+++ database/config.js
@@ -1,18 +1,47 @@
-//Acá esta toda la configuración para conectarse a la base de datos mongo
+// //Acá esta toda la configuración para conectarse a la base de datos mongo
 
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
-const dbConnection = async() => {
-    try {
-        await mongoose.connect( process.env.DB_CONNECTION );
-        console.log('Base de datos conectada');
+// const dbConnection = async() => {
+//     try {
+//         await mongoose.connect( process.env.DB_CONNECTION );
+//         console.log('Base de datos conectada');
 
-    } catch (error) {
+//     } catch (error) {
 
-        throw new Error('Error a la hora de inicializar base de datos');
-    }
-}
+//         throw new Error('Error a la hora de inicializar base de datos');
+//     }
+// }
+
+// module.exports = {
+//     dbConnection
+// };
+
+const mongoose = require( 'mongoose' );
+
+
+const dbConnection = async () => {
+
+  try {
+
+    await mongoose.connect( process.env.DB_CONNECTION, {
+      // useNewUrlParser: true, 
+      // useUnifiedTopology: true,
+      // useCreateIndex: true
+    } );
+
+    console.log( 'DB Online' );
+
+
+  } catch ( error ) {
+    console.log( error );
+    throw new Error( 'Error a la hora de inicializar BBDD' );
+  }
+
+
+};
+
 
 module.exports = {
-    dbConnection
+  dbConnection
 };
